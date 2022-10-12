7ae4ff558d526a4754ca3d47241b76e4aecf7768
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-12 14:17:58 +0100

    Created api to get all phones data

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const phones = require('./phones.json') //to get all phones data
 const port = 5000
 
 app.get('/', (req, res) => {
     res.send('this example server')
 });
+
+/* to show all phones data as API */
+app.get('/phones', (req, res) => {
+    res.send(phones)
+});
+
 
 app.listen(port, () => {
     console.log(`port ${port} is running`)
