aa65b5e0852502661b9f29eb913273709662259a
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-12 14:43:34 +0100

    Created api to get single phone data

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/phones', (req, res) => {
     res.send(phones)
 });
 
+/* to show phones data as per specific ID */
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log('looking for data for id:' id)
+
+    const phone = phones.find(ph => ph.id === id) || {};
+    res.send(phone);
+})
 
 app.listen(port, () => {
     console.log(`port ${port} is running`)
